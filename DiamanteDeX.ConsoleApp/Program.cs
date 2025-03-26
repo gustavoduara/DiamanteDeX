46ee91c9d51a57cb2209c386654e4cff96037b95
Gustavo Duara <gugaduara@gmail.com>
2025-03-25 21:57:41 -0300

    Add project files.

---
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DiamanteDeX.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            // Solicitar ao usuário um número inteiro ímpar
+            Console.Write("Digite um número inteiro ímpar: ");
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out numero))
+                {
+                    if (numero % 2 != 0)
+                    {
+                        Console.WriteLine("Você digitou o número ímpar: " + numero);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("O número não é ímpar. Tente novamente.");
+                        Console.Write("Digite um número inteiro ímpar: ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+                    Console.Write("Digite um número inteiro ímpar: ");
+                }
+            }
+        }
+    }
+}
